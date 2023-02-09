ba800b6c7495ee937ad939fabc65338e32c83262
triciahughes <hughes.a.tricia@gmail.com>
2023-02-09 15:02:31 -0800

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [dogImageData, setDogImageData] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((r) => r.json())
+      .then((data) => {
+        setDogImageData(data);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return <img src={dogImageData.message} alt="A Random Dog"></img>;
+}
+
+export default App;
